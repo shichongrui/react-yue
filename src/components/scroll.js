9b8004d57d37c75754ad5427176c9ce81e9f6d3d
Matt Sessions <msessions@instructure.com>
2018-06-25 15:27:08 -0600

    Make the scroll view content view sized based on its children

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -1,4 +1,4 @@
-const { Scroll } = require('gui')
+const { Scroll, Container } = require('gui')
 const Base = require('./base')
 const { warn } = require('./log')
 const { shouldUpdate, win32 } = require('../utils')
@@ -7,15 +7,17 @@ module.exports = class ScrollWrapper extends Base {
   constructor(props) {
     super(Scroll.create())
 
+    this.contentView = Container.create()
+    this.contentView.setStyle({ flex: 1 })
+    this._ele.setContentView(this.contentView)
+
     this.update(null, props)
   }
 
   update(lastProps, props) {
     super.update(lastProps, props)
 
-    if (shouldUpdate(props, lastProps, 'contentSize')) {
-      this._ele.setContentSize(props.contentSize)
-    }
+    this._ele.setContentSize(this.contentView.getPreferredSize())
 
     // TODO:
     if (props.hpolicy && props.vpolicy) {
@@ -27,12 +29,22 @@ module.exports = class ScrollWrapper extends Base {
     }
   }
 
-  addChildView(child) {
-    this._ele.setContentView(child._ele)
+  // I'm not sure why it is the case here and not anywhere else but
+  // in the case of Scroll these next 3 methods were losing their
+  // scoping so making them class properties and arrow functions
+  // will ensure they don't lose their scope
+  addChildView = (child) => {
+    this.contentView.addChildView(child._ele)
+    this._ele.setContentSize(this.contentView.getPreferredSize())
   }
 
-  // eslint-disable-next-line
-  insertBefore() {
-    warn('insertBefore of scroll in a non-op')
+  removeChildView = (child) => {
+    this.contentView.removeChildView(child._ele)
+    this._ele.setContentSize(this.contentView.getPreferredSize())
+  }
+
+  insertBefore = (child, beforeChild) => {
+    this.contentView.insertBefore(child._ele, beforeChild._ele)
+    this._ele.setContentSize(this.contentView.getPreferredSize())
   }
 }
